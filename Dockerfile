c4a04e7df8c5e6d0279d54c2e186a7d73328e030
gjelsas <gjelsas@users.noreply.github.com>
2022-03-30 17:32:14 +0200

    Added main.py and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.8-slim-buster
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+# Install pip requirements
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+RUN python -m pip install -r requirements.txt
+
+RUN apt update && apt install -y wget gnupg2 curl
+
+# Adding trusting keys to apt for repositories
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+# Adding Google Chrome to the repositories
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+# Updating apt to see and install Google Chrome
+RUN apt-get -y update
+
+# Magic happens
+RUN apt-get install -y google-chrome-stable
+
+# Installing Unzip
+RUN apt-get install -yqq unzip
+
+# Download the Chrome Driver
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+
+# Unzip the Chrome Driver into /usr/local/bin directory
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+# Set display port as an environment variable
+ENV DISPLAY=:99
+
+WORKDIR /app
+COPY . /app
+COPY ./geckodriver /app/geckodriver
+COPY ./env.py /app/env.py
+RUN mkdir -m 777 /home/appuser 
+
+# Creates a non-root user and adds permission to access the /app folder
+RUN useradd appuser && chown -R appuser /app
+USER appuser
+
+
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import smtplib
+from time import sleep
+from email.mime.text import MIMEText
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+# Secrets Import
+from env import username, password  # Fernuni Zugangsdaten
+from env import sender_mail as sender  # Versendender Mailserver
+from env import empfaenger 
+
+
+debug = 0
+
+chrome_options = Options()
+if not debug:
+    chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver.get("https://pos.fernuni-hagen.de/qisserver/rds?state=user&type=0&category=auth.logout")#put here the adress of your page
+elem = driver.find_element_by_id("loginForm:login")
+print(elem.get_attribute("class"))
+user = driver.find_element_by_id("asdf")
+pwd = driver.find_element_by_id("fdsa")
+user.send_keys(username)
+pwd.send_keys(password)
+elem.click()
+print("Loggin in...")
+verwaltung = driver.find_element_by_xpath("/html/body/div/div[5]/div[1]/ul/li/a")
+verwaltung.click()
+notenuebersicht = driver.find_element_by_xpath("/html/body/div/div[5]/div[2]/div/form/div/ul/li[3]/a")
+notenuebersicht.click()
+Leistungen = driver.find_element_by_xpath("/html/body/div/div[5]/div[2]/form/ul/li[2]/a[1]")
+Leistungen.click()
+leistungstabelle = "/html/body/div/div[5]/div[2]/form/table[2]"
+tabelle = driver.find_element_by_xpath(leistungstabelle).text
+tabelle_neu = driver.find_element_by_xpath(leistungstabelle).text
+print("starting the idle")
+while tabelle_neu == tabelle:
+    sleep(300)
+    driver.refresh()
+    print("Refreshing")
+    tabelle_neu = driver.find_element_by_xpath(leistungstabelle).text
+
+smtpObject = smtplib.SMTP_SSL(host=sender['host'], port=465)
+FROM = sender['emailaddress']
+smtpObject.login(FROM, sender['password'])
+x = empfaenger  # TODO needs testing if mail is send
+if len(tabelle_neu) > len(tabelle):        
+    try:
+        msg = MIMEText(
+            'Es sind Noten in der Tabelle hinzugefügt worden \n \n https://pos.fernuni-hagen.de/qisserver/rds?state=user&type=0&topitem= \n \n '+ tabelle_neu)
+        msg['Subject'] = 'Neue Noten bei der Fernuni'
+        msg['From'] = FROM
+        msg['To'] = x
+        smtpObject.sendmail(FROM, x, msg.as_string())
+        print('Successfully sent email')
+    except smtplib.SMTPException:
+        print("Error: unable to send email")
+else:
+    try:
+        msg = MIMEText(
+            'Etwas ist beim Notenchecker Script schief gegenagen')
+        msg['Subject'] = 'Fehler im Notenchecker Script'
+        msg['From'] = FROM
+        msg['To'] = x
+        smtpObject.sendmail(FROM, x, msg.as_string())
+        print('Successfully sent email')
+    except smtplib.SMTPException:
+        print("Error: unable to send email")
+driver.close()
