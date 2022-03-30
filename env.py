1ff7ecac780591770b0ea408512c57f34d9ec6a0
gjelsas <gjelsas@users.noreply.github.com>
2022-03-30 17:35:59 +0200

    added example env.py file

---
+++ env.py
@@ -0,0 +1,8 @@
+
+username = "q123456" # Fernuni Username
+password = "meinFeruniPasswort" # Fernuni Passwort
+
+sender_mail = {'emailaddress':'senderadresse@meinemail.server',
+    'password':'verysecureemailpasswort','host':'smtp.myemail.provider'} # Einstellungen des versendenden Emailservers
+
+empfaenger = "empfaenger@email.adresse"
